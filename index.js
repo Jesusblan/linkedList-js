d2c939bf6896b8de70500f2a2d50a2a8d53824d9
JesusBlan <jezublanco@gmail.com>
2025-02-24 20:30:45 +0100

    added a class to create new list and add the functions and a node class to add new nodes

---
+++ index.js
@@ -0,0 +1,12 @@
+class linkedList{
+    constructor(head = null){
+        this.head = head;
+    }
+}
+
+class listNode{
+    constructor(data, next = null){
+        this.data = data;
+        this.next = next;
+    }
+}
