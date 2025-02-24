ee6e03b32d6f8332c1e27ed98c04a1f24eb1a019
JesusBlan <jezublanco@gmail.com>
2025-02-24 20:49:10 +0100

    added some nodes a console logs to add and test the functions in the linkedList class

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ class listNode{
         this.next = next;
     }
 }
+
+
+let node1 = new listNode("cat");
+
+let list = new linkedList(node1);
+
+let node2 = new listNode("dog");
+
+let node3 = new listNode("parrot");
+
+node1.next = node2;
+node2.next = node3;
+
+
+console.log(list)
+console.log(node1)
